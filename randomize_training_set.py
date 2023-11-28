21bb8ba558e885d700bbda34fb779aae3784a329
PeteePanda <scrandispeter@gmail.com>
2023-11-27 22:25:57 -0500

    new best, cleaned up comments

--- randomize_training_set.py
+++ randomize_training_set.py
@@ -37,16 +37,7 @@ def populate_sets():
 
     # iterate through all pokemon, place 80% of each pokemons images in training set, 20% in testing set
     for pokemon in os.listdir(rotated_directory):
-        # images = os.listdir(rotated_directory + pokemon)
-        # random.shuffle(images)
-        # training_set = images[int(len(images) * training_ratio):]
-        # testing_set = images[:int(len(images) * training_ratio)]
         copy_tree(rotated_directory + pokemon, training_directory + pokemon, verbose=0)
-        # copy_tree(rotated_directory + pokemon, testing_directory + pokemon, verbose=0)
-        # for image in training_set:
-        #     os.remove(training_directory + pokemon + "/" + image)
-        # for image in testing_set:
-        #     os.remove(testing_directory + pokemon + "/" + image)
 
 
 def generate_directories():
